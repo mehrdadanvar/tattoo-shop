9406428c7cf0d0bafb2e712e0429f69a79cbbfbb
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-03-07 21:01:46 -0800

    added index page

---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="container w-3/4 mx-auto border border-slate-600 rounded-lg">
+      <div class="header">
+        <div
+          class="relative z-10 mx-36 bg-[radial-gradient(164.75%_100%_at_50%_0%,#334155_0%,#0F172A_48.73%)] py-10 px-5 shadow-lg sm:mx-0 sm:rounded-3xl sm:px-10"
+        >
+          <div class="flex flex-row">
+            <div>
+              <img
+                src="../assets/images/head1.jpg"
+                alt=""
+                width="500"
+                class="rounded-lg opacity-50"
+              />
+            </div>
+            <div>
+              <p class="text-white">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                iusto?
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="relative z-10 -mx-4 bg-[radial-gradient(164.75%_100%_at_50%_0%,#334155_0%,#0F172A_48.73%)] py-10 px-5 shadow-lg sm:mx-0 sm:rounded-3xl sm:px-10"
+          >
+            <div class="flex">
+              <div class="ml-4">
+                <h2 class="text-base font-semibold leading-7 text-white">
+                  Teams
+                </h2>
+                <p class="text-sm leading-6 text-slate-400">
+                  For product teams and agencies.
+                </p>
+              </div>
+            </div>
+            <div
+              class="relative -mx-5 mt-8 flex flex-col bg-slate-700/25 py-8 px-5 ring-1 ring-slate-700/50 sm:mx-0 sm:rounded-2xl"
+            >
+              <p class="flex items-center justify-center">
+                <span class="text-[2.5rem] leading-none text-white"
+                  >CA$<span class="font-bold">999</span></span
+                ><span class="ml-3 text-sm"
+                  ><span class="font-semibold text-white">one-time payment</span
+                  ><br /><span class="text-slate-400"
+                    >plus local taxes</span
+                  ></span
+                >
+              </p>
+              <p
+                class="order-last -mx-1 mt-4 flex justify-center text-sm leading-6 text-slate-400 sm:space-x-2"
+              >
+                <span class="hidden sm:inline">Lifetime access.</span
+                ><span class="hidden sm:inline">Unlimited projects.</span
+                ><span class="hidden sm:inline">Free updates.</span
+                ><span class="sm:hidden"
+                  >Includes lifetime access and free updates.</span
+                >
+              </p>
+              <a
+                class="inline-flex justify-center rounded-lg text-sm font-semibold py-2 px-3 bg-slate-600 text-white hover:bg-slate-500 mt-6 w-full"
+                href="https://tailwindui.com/checkout/cc42453b-bfcd-4a7c-8150-37946e36168e"
+                ><span>Get all-access for your team</span></a
+              >
+            </div>
+            <div class="mt-8 flex">
+              <div class="ml-6 text-sm leading-6 text-slate-400">
+                <p>
+                  <strong class="font-semibold text-slate-100"
+                    >Get access for your entire team</strong
+                  >
+                  — team licenses include access for up to 25 people to
+                  accommodate even the largest teams at your company.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
